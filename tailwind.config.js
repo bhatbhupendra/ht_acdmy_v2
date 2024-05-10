867081ed97df1e784fbf7823da7d53c0827ac0c1
Bhupendra Bhat <bhupendra420110bhat@gmail.com>
2024-05-10 22:56:41 +0545

    Footer added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         primary: "#3238f2",
+        footer_bg: "#f1f3f4",
       },
       fontFamily: {
         display: ["Poppins", "sans-serif"],
