1781a7469ffc645119e37d258a896eaabcd3117b
Seavam <rautshivam9@gmail.com>
2024-05-14 00:10:46 +0545

    Gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         card_bg: "#f1f9ff",
         logo_blue: "#019de6",
         blueish_dark: "#00234c",
+        navbar_color: "#e3e6f3",
+        footer_color:"#041e42"
+        
       },
       fontFamily: {
         display: ["Poppins", "sans-serif"],
