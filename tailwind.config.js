e90d67057e329ed3a0ed034aa04c01dca9393d8b
Bhupendra Bhat <bhupendra420110bhat@gmail.com>
2024-05-15 00:27:49 +0545

    Added: contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./home.html", "./gallary.html"],
+  content: ["./home.html", "./gallary.html", "./contact.html"],
   theme: {
     extend: {
       colors: {
